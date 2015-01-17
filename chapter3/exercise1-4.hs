9196380d667fc0438b49cb50ad66d05faca411c2
steves <sion.stevenson@wjec.co.uk>
2015-01-17 12:25:14 +0000

    Completed exercises for chapter 3.

--- chapter3/exercise1-4.hs
+++ chapter3/exercise1-4.hs
@@ -7,9 +7,9 @@ exOr x y = (x && not y ) || (not x && y)
 -- Exercise 3.3 Using literals on the left hand side
 
 exOr :: Bool -> Bool -> Bool
-exOr True True = False
-exOr True False = True
-exOr False True = True
+exOr True True   = False
+exOr True False  = True
+exOr False True  = True
 exOr False False = False
 
 -- Exercise 3.4 Two versions of the nAnd function
@@ -18,8 +18,8 @@ nAnd :: Bool -> Bool -> Bool
 nAnd x y = not (x && y)
 
 nAnd2 :: Bool -> Bool -> Bool
-nAnd2 True True = False
+nAnd2 True True   = False
 nAnd2 False False = True
-nAnd2 True False = True
+nAnd2 True False  = True
 nAnd2 False False = True
 
